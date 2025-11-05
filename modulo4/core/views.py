cfed9aa5eaa8506831c28252a5ccc0ad05254b62
codbyVitorDaniel <143666152+codbyVitorDaniel@users.noreply.github.com>
2025-11-05 09:47:52 -0300

    home html

--- modulo4/core/views.py
+++ modulo4/core/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Olá,Mundo! Esta e minha primeira pagina Django!</h1>")
-
+    return render(request, 'home.html')
 def site(request):
     return HttpResponse("<h1>Nova pasta</h1>")
