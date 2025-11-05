50db3200f3c11dbf68ec8c8405241c383cbc6c83
codbyVitorDaniel <143666152+codbyVitorDaniel@users.noreply.github.com>
2025-11-05 11:36:33 -0300

    adicionado ao context

--- modulo4/core/views.py
+++ modulo4/core/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    context={
+        'nome_usuario':'Vitor',
+        'tecnologias':['python','Django','HTML','CSS']
+    }
+    return render(request,'home.html',context)
+
 def site(request):
     return HttpResponse("<h1>Nova pasta</h1>")
+
