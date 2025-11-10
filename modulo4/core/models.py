c7b7b5cde5d104a4a0e9a34328f2cc6ddf602625
VitorDanielRC <143666152+VitorDanielRC@users.noreply.github.com>
2025-11-10 11:38:11 -0300

    models

--- modulo4/core/models.py
+++ modulo4/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Tarefa(models.Model):
+    titulo = models.CharField(max_length= 200)
+    concluida = models.BooleanField(default= False)
+    criado_em = models.DateTimeField(auto_now_add= True)
+
+    def __str__(self):
+        return self.titulo
+
+
+class execucao(models.Model):
+    nome = models.CharField(max_length= 200)
+    local = models.CharField(max_length= 200)
+    hora = models.DateTimeField(auto_now_add= True)
+    concluida = models.BooleanField(default = False)
+
+    def __str__(self):
+        return self.titulo
