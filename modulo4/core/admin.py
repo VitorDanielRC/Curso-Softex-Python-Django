1878d26c2c2081eaf6fc4cc8e21b51cbe32d62fa
VitorDanielRC <143666152+VitorDanielRC@users.noreply.github.com>
2025-11-10 11:38:02 -0300

    admin

--- modulo4/core/admin.py
+++ modulo4/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Tarefa
+from .models import execucao
 # Register your models here.
+admin.site.register(Tarefa)
+admin.site.register(execucao)
