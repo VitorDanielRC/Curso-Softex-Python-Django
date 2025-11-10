b6b9fb5b44506f09787abd68cb8e69a58587ded4
VitorDanielRC <143666152+VitorDanielRC@users.noreply.github.com>
2025-11-10 11:38:45 -0300

    view

--- modulo4/core/views.py
+++ modulo4/core/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Tarefa
+from .models import execucao
 # Create your views here.
 def home(request):
+    todas_as_tarefas = Tarefa.objects.all()
+    execucao_as = execucao.objects.all()
     context={
         'nome_usuario':'Vitor',
-        'tecnologias':['python','Django','HTML','CSS']
+        'tecnologias':['python','Django','HTML','CSS'],
+        'tarefas': todas_as_tarefas,
+        'exe' : execucao_as 
     }
     return render(request,'home.html',context)
 
