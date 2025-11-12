38456c229923d402ab9cc03ba8fe068ccbaaf57a
VitorDanielRC <143666152+VitorDanielRC@users.noreply.github.com>
2025-11-12 11:32:10 -0300

    vizualizaçao

--- modulo4/core/views.py
+++ modulo4/core/views.py
@@ -1,16 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Tarefa
 from .models import execucao
+from .form import TarefaForm
 # Create your views here.
 def home(request):
-    todas_as_tarefas = Tarefa.objects.all()
+    if request.method == 'POST':
+        form = TarefaForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = TarefaForm() # Cria um formulário vaz
+
+    todas_as_tarefas = Tarefa.objects.all().order_by('-criada_em')
     execucao_as = execucao.objects.all()
     context={
         'nome_usuario':'Vitor',
         'tecnologias':['python','Django','HTML','CSS'],
         'tarefas': todas_as_tarefas,
-        'exe' : execucao_as 
+        'exe' : execucao_as,
+        'form':form
     }
     return render(request,'home.html',context)
 
