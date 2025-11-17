21daae2851266157b745a3085c6fa7c658385373
VitorDanielRC <143666152+VitorDanielRC@users.noreply.github.com>
2025-11-17 10:46:00 -0300

    adicionado path novos

--- modulo4/core/urls.py
+++ modulo4/core/urls.py
@@ -7,8 +7,11 @@ urlpatterns = [
         views.home,
         name='home',),
     
-    path(
-        'site/',
-        views.site,
-        name='site',),
+    path('tarefa/<int:pk>/concluir/', views.concluir_tarefa, name='concluir_tarefa'),
+    
+    path('tarefa/<int:pk>/deletar/', views.deletar_tarefa, name='deletar_tarefa'),
+    
+    path('login/', views.login,name='login'),
 ]
+
+
