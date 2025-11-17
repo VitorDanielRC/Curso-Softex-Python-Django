a0ffdd69f70f55b1250c409f72bf37dcab3f6981
VitorDanielRC <143666152+VitorDanielRC@users.noreply.github.com>
2025-11-17 11:16:56 -0300

    views do sites

--- modulo4/core/views.py
+++ modulo4/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Tarefa
 from .models import execucao
@@ -14,7 +14,7 @@ def home(request):
     else:
         form = TarefaForm() # Cria um formulário vaz
 
-    todas_as_tarefas = Tarefa.objects.all().order_by('-criada_em')
+    todas_as_tarefas = Tarefa.objects.all().order_by('criada_em')
     execucao_as = execucao.objects.all()
     context={
         'nome_usuario':'Vitor',
@@ -28,3 +28,23 @@ def home(request):
 def site(request):
     return HttpResponse("<h1>Nova pasta</h1>")
 
+def concluir_tarefa(request, pk):
+# 1. Busca a tarefa pela 'pk' (ID) vinda da URL.
+# Se não achar, retorna um erro 404.
+    tarefa = get_object_or_404(Tarefa, pk=pk)
+# 2. Segurança: Apenas execute se o método for POST
+    if request.method == 'POST':
+# 3. A Lógica de "Update"
+        tarefa.concluida = True
+        tarefa.save() # Não se esqueça de salvar!
+# 4. Redireciona de volta para a 'home' (Padrão PRG)
+    return redirect('home')
+def deletar_tarefa(request, pk):
+# 1. Busca a tarefa
+    tarefa = get_object_or_404(Tarefa, pk=pk)
+# 2. Segurança: Apenas execute se o método for POST
+    if request.method == 'POST':
+# 3. A Lógica de "Delete"
+        tarefa.delete()
+# 4. Redireciona de volta para a 'home'
+    return redirect('home')
