2e79b74bb2ce4ee4e0a6a87ea0b0f86ec0ddf1c9
VitorDanielRC <143666152+VitorDanielRC@users.noreply.github.com>
2025-11-19 11:53:31 -0300

    adicionado cascade

--- modulo4/core/models.py
+++ modulo4/core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 class Tarefa(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     titulo = models.CharField(max_length= 200)
     concluida = models.BooleanField(default= False)
     criada_em = models.DateTimeField(auto_now_add= True)
@@ -16,4 +17,4 @@ class execucao(models.Model):
     concluida = models.BooleanField(default = False)
 
     def __str__(self):
-        return self.titulo
+        return self.nome
