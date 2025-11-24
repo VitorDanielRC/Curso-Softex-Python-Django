f1de853503a7aff2ba25e2faaace5ecd022d17ed
VitorDanielRC <143666152+VitorDanielRC@users.noreply.github.com>
2025-11-24 10:42:17 -0300

    dicionado filtro no admin

--- modulo4/core/admin.py
+++ modulo4/core/admin.py
@@ -4,6 +4,7 @@ from .models import Tarefa
 class TarefaAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'user', 'concluida', 'criada_em')
     list_filter = ('concluida', 'user', 'criada_em')
+    search_fields = ('titulo', 'user__username')
 admin.site.register(Tarefa, TarefaAdmin)
 
 # Register your models here.
