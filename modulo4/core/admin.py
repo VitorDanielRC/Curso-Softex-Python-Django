b92eca0003d22677b2c38fc9a139aadfa5292f8b
VitorDanielRC <143666152+VitorDanielRC@users.noreply.github.com>
2025-11-24 10:00:10 -0300

    adicionado admin

--- modulo4/core/admin.py
+++ modulo4/core/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Tarefa
 
+class TarefaAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'user', 'concluida', 'criada_em')
+    list_filter = ('concluida', 'user', 'criada_em')
+admin.site.register(Tarefa, TarefaAdmin)
+
 # Register your models here.
-admin.site.register(Tarefa)
