9999e174e4d28f50949706896202c8309b403ac0
VitorDanielRC <143666152+VitorDanielRC@users.noreply.github.com>
2025-11-24 09:53:36 -0300

    urls

--- modulo4/core/urls.py
+++ modulo4/core/urls.py
@@ -2,17 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(
-        '',
-        views.home,
-        name='home',),
-    
+    path('', views.home, name='home'),
     path('tarefa/<int:pk>/concluir/', views.concluir_tarefa, name='concluir_tarefa'),
-    
     path('tarefa/<int:pk>/deletar/', views.deletar_tarefa, name='deletar_tarefa'),
-    
-    path('register', view.register, name='register')
-
+    path('register/', views.register, name='register'),
 ]
-
-
