a291fddeb5849c11f6e0fa641b9ad9b3f93bf2b6
VitorDanielRC <143666152+VitorDanielRC@users.noreply.github.com>
2025-11-24 09:53:21 -0300

    models

--- modulo4/core/models.py
+++ modulo4/core/models.py
@@ -1,20 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 class Tarefa(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    titulo = models.CharField(max_length= 200)
-    concluida = models.BooleanField(default= False)
-    criada_em = models.DateTimeField(auto_now_add= True)
+    titulo = models.CharField(max_length=200) # equivale a TEXT no SQL
+    concluida = models.BooleanField(default=False) # equivale a bool
+    criada_em = models.DateTimeField(auto_now_add=True) 
 
     def __str__(self):
         return self.titulo
-
-
-class execucao(models.Model):
-    nome = models.CharField(max_length= 200)
-    local = models.CharField(max_length= 200)
-    hora = models.DateTimeField(auto_now_add= True)
-    concluida = models.BooleanField(default = False)
-
-    def __str__(self):
-        return self.nome
