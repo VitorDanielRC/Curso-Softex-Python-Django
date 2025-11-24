fa8cc58704192766364b7956f8429159c3ecaa96
VitorDanielRC <143666152+VitorDanielRC@users.noreply.github.com>
2025-11-24 11:03:58 -0300

    adicionado emails

--- modulo4/core/admin.py
+++ modulo4/core/admin.py
@@ -5,6 +5,21 @@ class TarefaAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'user', 'concluida', 'criada_em')
     list_filter = ('concluida', 'user', 'criada_em')
     search_fields = ('titulo', 'user__username')
+    fieldsets = (
+('Informações Principais', {
+'fields': ('user', 'titulo')
+}),
+('Status da Tarefa', {
+'fields': ('concluida', 'criada_em')
+}),
+)
+# 'readonly_fields' define campos que podem ser vistos, mas não editados
+# Perfeito para campos automáticos como 'criada_em'
+readonly_fields = ('criada_em',)
+
+@admin.display(description='Email do Usuário') # Define o título da coluna
+def get_user_email(self, obj):
+    return obj.user.email
 admin.site.register(Tarefa, TarefaAdmin)
 
 # Register your models here.
