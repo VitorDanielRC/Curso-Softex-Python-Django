f0e7067b7ef7270a8a09b2516c184355a6fc99af
VitorDanielRC <143666152+VitorDanielRC@users.noreply.github.com>
2025-11-26 11:36:12 -0300

    mudado o login

--- modulo4/core/views.py
+++ modulo4/core/views.py
@@ -8,25 +8,26 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def home(request):
     if request.method == 'POST':
-        form = TarefaForm(request.POST)
+# 1. Passe o 'user' logado para o formulário no POST
+        form = TarefaForm(request.POST, user=request.user)
         if form.is_valid():
             tarefa = form.save(commit=False)
             tarefa.user = request.user
             tarefa.save()
+# O 'project' já é salvo automaticamente pelo form.save()
             return redirect('home')
-    else:
-        form = TarefaForm()
-
+        else:
+# 2. Passe o 'user' logado para o formulário no GET
+            form = TarefaForm(user=request.user)
     todas_as_tarefas = Tarefa.objects.filter(user=request.user).order_by('-criada_em')
 
     context = {
     'nome_usuario': request.user.username,
-    'tecnologias': ['Autenticação', 'ForeignKey', 'Login'],
     'tarefas': todas_as_tarefas,
     'form': form,
     }
+    
     return render(request, 'home.html', context)
-
 @login_required
 def concluir_tarefa(request, pk):
     tarefa = get_object_or_404(Tarefa, pk=pk, user=request.user)
