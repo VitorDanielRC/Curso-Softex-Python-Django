3096db3f8464eab54bcbbbb2647aa915d1a23846
vitordanielrosa <vitorcabral2004@gmail.com>
2025-12-09 14:49:24 -0300

    feat: add URL patterns for Tarefa statistics and soft delete views

--- modulo5/core/urls.py
+++ modulo5/core/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import ListaTarefasAPIView
+from .views import (ListaTarefasAPIView, TarefaEstatisticasAPIView, TarefaSoftDeleteAPIView)
 
 app_name = 'core'
 urlpatterns = [
     path('tarefas/', ListaTarefasAPIView.as_view(), name='lista-tarefas'),
+    path('tarefas/estatisticas/', TarefaEstatisticasAPIView.as_view(), name='tarefas-estatisticas'),
+    path('tarefas/<int:pk>/soft_delete/', TarefaSoftDeleteAPIView.as_view(), name='tarefa-soft-delete'),
+    
 ]
