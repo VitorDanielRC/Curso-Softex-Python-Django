358f9478d83cbd3df04d3cf4f05f2776baefdb04
VitorDanielRC <143666152+VitorDanielRC@users.noreply.github.com>
2025-12-10 10:56:30 -0300

    feature/adicionado delete

--- modulo5/core/urls.py
+++ modulo5/core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import (ListaTarefasAPIView, TarefaEstatisticasAPIView, TarefaSoftDeleteAPIView)
+from .views import (ListaTarefasAPIView, TarefaEstatisticasAPIView, TarefaSoftDeleteAPIView, DetalheTarefaAPIView)
 
 app_name = 'core'
 urlpatterns = [
     path('tarefas/', ListaTarefasAPIView.as_view(), name='lista-tarefas'),
     path('tarefas/estatisticas/', TarefaEstatisticasAPIView.as_view(), name='tarefas-estatisticas'),
     path('tarefas/<int:pk>/soft_delete/', TarefaSoftDeleteAPIView.as_view(), name='tarefa-soft-delete'),
+    path('tarefas/<int:pk>/',DetalheTarefaAPIView.as_view(),name='detalhe-tarefa'),
+
     
 ]
