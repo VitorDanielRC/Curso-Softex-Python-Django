1c410f99d495ced3a23653d22ff2912a580bd490
vitordanielrosa <vitorcabral2004@gmail.com>
2025-12-13 20:08:32 -0300

    feature: update URL routing to include TarefaViewSet with DefaultRouter

--- modulo5/core/urls.py
+++ modulo5/core/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import (ListaTarefasAPIView, TarefaEstatisticasAPIView, TarefaSoftDeleteAPIView, DetalheTarefaAPIView)
+from rest_framework.routers import DefaultRouter
+from .views import TarefaViewSet
 
 app_name = 'core'
 urlpatterns = [
@@ -7,6 +9,10 @@ urlpatterns = [
     path('tarefas/estatisticas/', TarefaEstatisticasAPIView.as_view(), name='tarefas-estatisticas'),
     path('tarefas/<int:pk>/soft_delete/', TarefaSoftDeleteAPIView.as_view(), name='tarefa-soft-delete'),
     path('tarefas/<int:pk>/',DetalheTarefaAPIView.as_view(),name='detalhe-tarefa'),
-
-    
 ]
+
+
+router = DefaultRouter()
+router.register(r"tarefas", TarefaViewSet, basename="tarefas")
+
+urlpatterns = router.urls
