9989be0453f30742750b6b0cc93c30ff37c84a24
vitordanielrosa <vitorcabral2004@gmail.com>
2025-12-19 16:39:35 -0300

    feat: add IsAdminOrOwner permission class for object-level access control

---
+++ modulo5/core/permissions.py
@@ -0,0 +1,12 @@
+from rest_framework import permissions
+
+
+class IsAdminOrOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if not request.user or not request.user.is_authenticated:
+            return False
+
+        if request.user.is_staff:
+            return True
+
+        return obj.user == request.user
