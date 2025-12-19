ce580013af3be0c8c75510d23b6735abca5a9b3c
vitordanielrosa <vitorcabral2004@gmail.com>
2025-12-19 16:39:53 -0300

    refactor: clean up urls.py by removing unused paths and views

--- modulo5/core/urls.py
+++ modulo5/core/urls.py
@@ -1,26 +1,23 @@
 from django.urls import path
-from .views import (ListaTarefasAPIView, TarefaEstatisticasAPIView, TarefaSoftDeleteAPIView, DetalheTarefaAPIView)
-from rest_framework.routers import DefaultRouter
-from .views import TarefaViewSet
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
-
+from .views import (
+    ListaTarefasAPIView,
+    DetalheTarefaAPIView,
+    TarefasEstatisticasAPIView,
+    MinhaView,
+    LogoutView,
+    MeView,
+    ChangePasswordView,
+    StatsView,
+)
 
 app_name = 'core'
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/token/',TokenObtainPairView.as_view(),name='token_obtain_pair'),
-    path('api/token/refresh/',TokenRefreshView.as_view(),name='token_refresh'),
-    path('api/', include('core.urls')),
     path('tarefas/', ListaTarefasAPIView.as_view(), name='lista-tarefas'),
-    path('tarefas/estatisticas/', TarefaEstatisticasAPIView.as_view(), name='tarefas-estatisticas'),
-    path('tarefas/<int:pk>/soft_delete/', TarefaSoftDeleteAPIView.as_view(), name='tarefa-soft-delete'),
-    path('tarefas/<int:pk>/',DetalheTarefaAPIView.as_view(),name='detalhe-tarefa'),
+    path('tarefas/<int:pk>/', DetalheTarefaAPIView.as_view(), name='detalhe-tarefa'),
+    path('teste/', MinhaView.as_view(), name='teste-autenticado'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('me/', MeView.as_view(), name='me'),
+    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
+    path('stats/', StatsView.as_view(), name='stats'),
 ]
-
-
-router = DefaultRouter()
-router.register(r"tarefas", TarefaViewSet, basename="tarefas")
-
-urlpatterns = router.urls
