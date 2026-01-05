64f1bc4d75517a23bf44622a0b9d21f8c7ba584d
VitorDanielRC <143666152+VitorDanielRC@users.noreply.github.com>
2026-01-05 11:59:05 -0300

    settings

--- modulo5/config/settings.py
+++ modulo5/config/settings.py
@@ -13,6 +13,7 @@ import os
 import environ
 from pathlib import Path
 from datetime import timedelta
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -111,6 +112,11 @@ DATABASES = {
     }
 }
 
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': ':memory:', # Banco reside apenas na RAM
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
